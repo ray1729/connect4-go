f7497e91e6987a589ae0c05db9c7b82f518bac10
Ray Miller <ray@1729.org.uk>
2017-05-27 15:27:21 +0100

    Add players.

--- connect4.go
+++ connect4.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-    "bytes"
-    "fmt"
-//    "log"
-
-//    "github.com/pkg/errors"
+  "bytes"
+  "fmt"
+  "math/rand"
+  "time"
 )
 
 type Player int
 
 const (
-    Nobody Player = 0
-    Red           = 1
-    Yellow        = 2
+  Nobody Player = 0
+  Red           = 1
+  Yellow        = 2
 )
 
 const boardHeight = 6
@@ -46,27 +45,24 @@ func (s *GameState) PlayerAt(row, col int) Player {
 }
 
 func (s *GameState) CanPlay(col int) bool {
-    if len(s.Columns[col]) < boardHeight {
-        return true
-    }
-    return false
+  return col >= 0 && col < boardWidth && len(s.Columns[col]) < boardHeight
 }
 
 func (s *GameState) ValidMoves() []int {
-    var res []int
-    for i := 0; i < boardWidth; i++ {
-        if s.CanPlay(i) {
-            res = append(res, i)
-        }
+  var res []int
+  for i := 0; i < boardWidth; i++ {
+    if s.CanPlay(i) {
+      res = append(res, i)
     }
-    return res
+  }
+  return res
 }
 
 func TogglePlayer(p Player) Player {
-    if p == Red {
-        return Yellow
-    }
-    return Red
+  if p == Red {
+    return Yellow
+  }
+  return Red
 }
 
 func (s *GameState) MakeMove(col int) *GameState {
@@ -85,18 +81,22 @@ func (s *GameState) MakeMove(col int) *GameState {
   return res
 }
 
+func token(p Player) string {
+  if p == Red {
+    return "o"
+  }
+  if p == Yellow {
+    return "x"
+  }
+  return "."
+}
+
 func (s *GameState) String() string {
   var buffer bytes.Buffer
   for r := boardHeight - 1; r >= 0; r-- {
     for c := 0; c < boardWidth; c++ {
-      switch s.PlayerAt(r, c) {
-      case Nobody:
-        buffer.WriteString(".")
-      case Red:
-        buffer.WriteString("o")
-      case Yellow:
-        buffer.WriteString("x")
-      }
+      p := s.PlayerAt(r, c)
+      buffer.WriteString(token(p))
       if c < boardWidth-1 {
         buffer.WriteString(" ")
       }
@@ -136,12 +136,115 @@ func winningMoveInDirection(s *GameState, col, dx, dy int) bool {
   return false
 }
 
+type Mover interface {
+  NextMove(s *GameState) int
+}
+
+type RandomMover struct{}
+
+func (m *RandomMover) NextMove(s *GameState) int {
+  var winning, losing, neutral []int
+MOVE:
+  for _, move := range s.ValidMoves() {
+    t := s.MakeMove(move)
+    if t.Winner != Nobody {
+      winning = append(winning, move)
+      continue MOVE
+    }
+    for _, opponent_move := range t.ValidMoves() {
+      u := t.MakeMove(opponent_move)
+      if u.Winner != Nobody {
+        losing = append(losing, move)
+        continue MOVE
+      }
+    }
+    neutral = append(neutral, move)
+  }
+  if len(winning) > 0 {
+    i := rand.Intn(len(winning))
+    return winning[i]
+  }
+  if len(neutral) > 0 {
+    i := rand.Intn(len(neutral))
+    return neutral[i]
+  }
+  i := rand.Intn(len(losing))
+  return losing[i]
+}
+
+type ConsoleMover struct{}
+
+func (m *ConsoleMover) NextMove(s *GameState) int {
+  fmt.Print(s)
+  fmt.Println("0 1 2 3 4 5 6")
+  fmt.Printf("%s to play. Enter your move: ", token(s.NextPlayer))
+  var i int
+  for {
+    _, err := fmt.Scan(&i)
+    if err == nil && s.CanPlay(i) {
+      break
+    }
+    fmt.Print("Please enter a valid move: ")
+  }
+  return i
+}
+
+type MonteCarloMover struct {
+  level int
+}
+
+func (m *MonteCarloMover) NextMove(s *GameState) int {
+  scores := make(map[int]int)
+  randomMover := new(RandomMover)
+  for _, move := range s.ValidMoves() {
+    score := 0
+    for i := 0; i < m.level; i++ {
+      t := GameLoop(randomMover, randomMover, s.MakeMove(move))
+      if t.Winner == s.NextPlayer {
+        score += 1
+      } else if t.Winner == TogglePlayer(s.NextPlayer) {
+        score -= 1
+      }
+    }
+    scores[move] = score
+  }
+  first := true
+  var bestMove, bestScore int
+  for move, score := range scores {
+    if first {
+      bestMove = move
+      bestScore = score
+      first = false
+      continue
+    }
+    if score > bestScore {
+      bestMove = move
+      bestScore = score
+    }
+  }
+  return bestMove
+}
+
+func GameLoop(red, yellow Mover, s *GameState) *GameState {
+  for !s.IsGameOver {
+    if s.NextPlayer == Red {
+      s = s.MakeMove(red.NextMove(s))
+    } else {
+      s = s.MakeMove(yellow.NextMove(s))
+    }
+  }
+  return s
+}
+
 func main() {
-  s := &GameState{NextPlayer: Red}
+  rand.Seed(time.Now().UnixNano())
+  p1 := new(ConsoleMover)
+  p2 := &MonteCarloMover{level: 50}
+  s := GameLoop(p1, p2, &GameState{NextPlayer: Red})
   fmt.Println(s)
-  fmt.Println(s.ValidMoves())
-  for i := 0; i < boardHeight; i++ {
-    s  = s.MakeMove(0)
-    fmt.Println(s)
+  if s.Winner == Nobody {
+    fmt.Println("Game over! It's a draw.")
+  } else {
+    fmt.Printf("Game over! %s won\n", token(s.Winner))
   }
 }
